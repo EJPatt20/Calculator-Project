a4bbcbd8bf7f5d191ee5fd1dd3280dcb403d8228
EJ <everett.patterson@rallyhealth.com>
2021-05-14 11:55:18 -0400

    Resolving PR comments

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.calculator.adapters.CalculatorGridAdapter
-import com.example.calculator.data.CalculatorComponent
+import com.example.calculator.data.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,29 +15,29 @@ class MainActivity : AppCompatActivity() {
 
         val numberGrid = findViewById<View>(R.id.calc_grid) as RecyclerView
         val calcComponents = mutableListOf<CalculatorComponent>()
-        calcComponents.add(CalculatorComponent("C"))
-        calcComponents.add(CalculatorComponent("( )"))
-        calcComponents.add(CalculatorComponent("%"))
-        calcComponents.add(CalculatorComponent("⬅"))
+        calcComponents.add(ClearButtonComponent())
+        calcComponents.add(ParenthesisButtonComponent())
+        calcComponents.add(PercentButtonComponent())
+        calcComponents.add(PlusButtonComponent())
         for (i in 1..3) {
-            val number = CalculatorComponent(i.toString())
+            val number = NumberButtonComponent(i.toString())
             calcComponents.add(number)
         }
-        calcComponents.add(CalculatorComponent("÷"))
+        calcComponents.add(DivideButtonComponent())
         for (i in 4..6) {
-            val number = CalculatorComponent(i.toString())
+            val number = NumberButtonComponent(i.toString())
             calcComponents.add(number)
         }
-        calcComponents.add(CalculatorComponent("×"))
+        calcComponents.add(MultiplyButtonComponent())
         for (i in 7..9) {
-            val number = CalculatorComponent(i.toString())
+            val number = NumberButtonComponent(i.toString())
             calcComponents.add(number)
         }
-        calcComponents.add(CalculatorComponent("-"))
-        calcComponents.add(CalculatorComponent("0"))
-        calcComponents.add(CalculatorComponent("."))
-        calcComponents.add(CalculatorComponent("+/-"))
-        calcComponents.add(CalculatorComponent("="))
+        calcComponents.add(MinusButtonComponent())
+        calcComponents.add(NumberButtonComponent("0"))
+        calcComponents.add(DecimalButtonComponent())
+        calcComponents.add(SignButtonComponent())
+        calcComponents.add(EqualsButtonComponent())
         numberGrid.adapter = CalculatorGridAdapter(calcComponents)
         numberGrid.layoutManager = GridLayoutManager(this, 4)
     }
--- app/src/main/java/com/example/calculator/data/CalculatorModel.kt
+++ app/src/main/java/com/example/calculator/data/CalculatorModel.kt
@@ -1,5 +1,61 @@
 package com.example.calculator.data
 
-data class CalculatorComponent(
-    var value: String
+abstract class CalculatorComponent(
+    open val value: String,
 )
+
+data class PlusButtonComponent(
+    override val value: String = "+",
+
+) : CalculatorComponent(value)
+
+data class MinusButtonComponent(
+    override val value: String = "-",
+
+) : CalculatorComponent(value)
+
+data class DivideButtonComponent(
+    override val value: String = "÷",
+
+) : CalculatorComponent(value)
+
+data class MultiplyButtonComponent(
+    override val value: String = "×",
+
+) : CalculatorComponent(value)
+
+data class PercentButtonComponent(
+    override val value: String = "%",
+
+) : CalculatorComponent(value)
+
+data class ParenthesisButtonComponent(
+    override val value: String = "%",
+
+) : CalculatorComponent(value)
+
+data class ClearButtonComponent(
+    override val value: String = "⬅",
+
+    ) : CalculatorComponent(value)
+
+data class DecimalButtonComponent(
+    override val value: String = ".",
+
+    ) : CalculatorComponent(value)
+
+data class SignButtonComponent(
+    override val value: String = "+/-",
+
+    ) : CalculatorComponent(value)
+
+data class EqualsButtonComponent(
+    override val value: String = "=",
+
+    ) : CalculatorComponent(value)
+
+data class NumberButtonComponent(
+    override val value: String,
+    val intVal: Int = value.toInt()
+
+    ) : CalculatorComponent(value)
